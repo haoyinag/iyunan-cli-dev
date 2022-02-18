53d3cb0ab563a4dca88c27ecdc27bab519dd90ce
郁南 <haoyinag0@hotmail.com>
2022-02-18 08:02:12 +0800

    Update index.js

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2022-02-17 08:02:59
+ * @LastEditTime: 2022-02-18 08:02:05
+ * @FilePath: /iyunan-cli/packages/core/lib/index.js
+ * @Description: 
+ */
 'use strict';
 
 module.exports = core;
 
 function core() {
     // TODO
+    console.log('core');
 }
