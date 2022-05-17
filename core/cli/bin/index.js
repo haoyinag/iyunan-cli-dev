086572dcfd5eb4bd060fad8bc6c5d8ac2c3f32cc
郁南 <haoyinag0@hotmail.com>
2022-05-17 17:36:10 +0800

    1

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -2,12 +2,12 @@
 
 const importLocal = require("import-local");
 
-console.log(123,importLocal(__filename));
+// console.log(123,importLocal(__filename));
 
 if (importLocal(__filename)) {
-  console.log(111);
+  // console.log(111);
   require("npmlog").info("cli", "正在使用 iyunan-cli 本地版本");
 } else {
-  console.log(222);
+  // console.log(222);
   require("../lib")(process.argv.slice(2));
 }
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,19 +2,24 @@
  * @Author: 郁南
  * @LastEditors: 郁南
  * @Date: 2022-02-17 08:02:59
- * @LastEditTime: 2022-05-15 23:17:16
+ * @LastEditTime: 2022-05-17 11:00:47
  * @FilePath: /iyunan-cli/core/cli/lib/index.js
  * @Description:
  */
 "use strict";
+const pkg = require("../package.json");
 
 module.exports = core;
 
-("use strict");
-
-module.exports = core;
+/**
+ * reqiure解释module的集中方式：
+ * 1、加载 .js/.json/.node 三种方式文件
+ * 2、加载保函 module.expots/exports
+ * 3、加载：.json==>JOSN.parse( )
+ */
 
 function core() {
   // TODO
-  console.log("useing core 123");
+  const version = pkg.version;
+  console.log(version);
 }
