383f8fe9c414e845f30a7cff2bcd675bb546cc95
郁南 <haoyinag0@hotmail.com>
2022-05-19 19:47:29 +0800

    use commander

---
+++ core/use-commander/lib/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2022-05-19 18:33:56
+ * @LastEditTime: 2022-05-19 18:35:54
+ * @FilePath: /iyunan-cli/core/use-commander/lib/index.js
+ * @Description: 
+ */
+'use strict';
+const commander = require('commander');
+
+module.exports = useCommander;
+
+function useCommander() {
+    // TODO
+    console.log('use-commander');
+}
+useCommander() 
